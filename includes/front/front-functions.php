4c4e8d77d80ae4f55b85d67164e4562ce5b95002
rvst312 <aaronchmanda25@gmail.com>
2024-01-11 12:31:41 +0100

    new

--- includes/front/front-functions.php
+++ includes/front/front-functions.php
@@ -33,6 +33,29 @@ function display_folders()
     <button type="button" class="folders" data-valor="diamante">
         DIAMANTE
     </button>
+
+    <script>
+        // Lógica de manejo de las carpetas en el cliente. Se envia un valor al servidor en cada petición, el cual se usa de parametro
+        document.addEventListener('DOMContentLoaded', function () {
+
+            const folders = document.getElementsByClassName('folders');
+
+            for (let i = 0; i < folders.length; i++) {
+
+                folders[i].addEventListener('click', function () {
+                    console.log('ready click en' + folders[i])
+
+                    let value = this.getAttribute('data-valor');
+                    // Realiza una solicitud AJAX al servidor
+                    let xhr = new XMLHttpRequest();
+                    xhr.open('POST', '<?php echo admin_url('admin-ajax.php'); ?>', true);
+                    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+                    xhr.send('action=process_click&value=' + encodeURIComponent(value));
+                });
+            }
+        });
+
+    </script>
     <?php
 }
 
