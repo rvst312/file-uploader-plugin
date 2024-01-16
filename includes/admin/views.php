df3712b6d09d94d8bea669954bb7e73dc2b86dd4
rvst312 <aaronchmanda25@gmail.com>
2024-01-16 12:11:01 +0100

    new

--- includes/admin/views.php
+++ includes/admin/views.php
@@ -23,4 +23,36 @@ function render_interface()
 
     view_upload_form($current_path);
     display_uploaded_files($current_path); 
+
+    ?>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+    // Obtén el botón y el campo de entrada
+    var pathInput = document.getElementById('pathInput');
+
+    // Función para cambiar la ruta
+    function changePath(newPath) {
+        // Construye la nueva URL con la nueva ruta
+        var currentUrl = window.location.href.split('?')[0];
+        var newUrl = currentUrl + '?path=' + encodeURIComponent(newPath);
+
+        // Redirige a la nueva URL
+        window.location.href = newUrl;
+    }
+
+    // Agrega eventos clic a los botones de cambio de ruta
+    document.getElementById('changePathButton1').addEventListener('click', function () {
+        changePath('oro');
+    });
+
+    document.getElementById('changePathButton2').addEventListener('click', function () {
+        changePath('reloj');
+    });
+
+    document.getElementById('changePathButton3').addEventListener('click', function () {
+        changePath('diamantes')
+    });
+});
+    </script>
+    <?php
 }
