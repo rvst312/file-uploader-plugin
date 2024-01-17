2f55f0edc6d71beb1fe753517c66aeb062a76807
rvst312 <aaronchmanda25@gmail.com>
2024-01-17 11:08:31 +0100

    new

--- includes/front/front-functions.php
+++ includes/front/front-functions.php
@@ -26,9 +26,39 @@ function display_front()
     ?>
     <div class="wrap">
         <h1>Subir Archivos</h1>
-        <button class="button change-view-button" data-param1="oro" data-param2="oro">ORO</button>
-        <button class="button change-view-button" data-param1="reloj" data-param2="reloj">RELOJ</button>
-        <button class="button change-view-button" data-param1="diamantes" data-param2="diamantes">DIAMANTES</button>
+        <div class="folder">
+            <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30"
+                viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                <path
+                    d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z" />
+            </svg>
+            <button class="button change-view-button" data-param1="oro" data-param2="oro">
+                ORO
+            </button>
+        </div>
+
+        <div class="folder">
+            <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30"
+                viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                <path
+                    d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z" />
+            </svg>
+            <button class="button change-view-button" data-param1="reloj" data-param2="reloj">
+                RELOJ
+            </button>
+        </div>
+
+        <div class="folder">
+            <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30"
+                viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                <path
+                    d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z" />
+            </svg>
+            <button class="button change-view-button" data-param1="diamantes" data-param2="diamantes">
+                DIAMANTES
+            </button>
+        </div>
+
         <div id="dynamic-content">
             <?php
             // Cargamos dinámicamente el contenido
